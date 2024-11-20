bc149e3d48a6f9168b08185a375fef03e12d8e85
AndrikOrt432 <andrikmarquez8@gmail.com>
2024-11-20 08:19:48 -0600

    Version_02: Lista foreach, for, clear, sort, orferby.

--- Enumerable.UI.Consola/Program.cs
+++ Enumerable.UI.Consola/Program.cs
@@ -8,8 +8,42 @@ namespace Enumerable.UI.Consola
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("=====================");
+            Console.WriteLine("Enumerable");
+            Console.WriteLine("=====================");
+
+            #region Lista
+
+            var lista = new List<string>
+            {
+                "Juan",
+                "Ileny",
+                "Jan",
+                "Dania",
+                "Jose",
+                "Carlos",
+                "Karla",
+                "Zuri"
+            };
+
+            lista.Add("Sonia");
+
+            //foreach (var nombre in lista)
+            //{
+            //    Console.WriteLine(nombre);
+            //}
+
+            lista.Order();
+
+            for (int i = 0; i < lista.Count; i++) 
+            {
+                Console.WriteLine(lista[i]);            
+            }
+
+            #endregion
+            Console.ReadKey();
         }
     }
 }
